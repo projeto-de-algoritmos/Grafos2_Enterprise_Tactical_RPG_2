1c62096acb4b20937949945d1bc7ae0330aedb50
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2021-08-23 02:48:51 -0300

    Implementar Dijkstra na matriz
    - adicionar método que usa Dijkstra para encontrar o caminho mais curto
      na matriz
    - modificar construtor da classe de matriz para acomodar custos
      genéricos e os métodos necessários para se executar o Dijkstra
    - adicionar método privado para transformar matriz em lista de posições
      visitáveis para uso no Dijkstra
    - criar classe que retorna caminho e custo
    - alterar método equals e hash da classe Position
    - adicionar e modificar testes da classe de matriz genérica com custos
    - fazer inimigos e jogador encontrarem o caminho usando Dijkstra
    - adicionar mais custos aleatórios
    - impedir jogador de ir em diretamente a um inimigo (não conseguirá
      entrar na mesma posição)
    - impedir inimigos de entrarem um no outro, forçando-os a cercar o
      jogador por múltiplos caminhos
    - reformatar código

--- src/game/Panel.java
+++ src/game/Panel.java
@@ -8,15 +8,18 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.function.BinaryOperator;
 
 import javax.swing.JPanel;
 
 import game.entities.Enemy;
 import game.entities.Entity;
 import game.entities.Player;
+import graphs.CheapestPath;
 import graphs.GraphMatrix;
 import graphs.Position;
 
@@ -24,6 +27,8 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 
 	private static final long serialVersionUID = 1L;
 	private static final Integer FORBIDDEN = -1;
+	private static final Integer EMPTY = 0;
+	private static final Integer VISITED = 1;
 	private static int WIDTH = 500;
 	private static int HEIGHT = 500;
 	private Entity player;
@@ -35,11 +40,30 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 	private int lastMouseY;
 	private boolean inPlayer;
 
-	private final int initialCost = 0;
+	private final int initialCost = 1;
+	private final int minimumCost = 0;
+	private final int maximumCost = Integer.MAX_VALUE;
 
 	private List<Enemy> enemies = new ArrayList<Enemy>();
 
+	final private int sizeX = 20;
+	final private int sizeY = 20;
 	final private int playerMoves = 5;
+	final private Comparator<Integer> costComparator = new Comparator<Integer>() {
+		@Override
+		public int compare(Integer o1, Integer o2) {
+			if (o1 < o2) {
+				return -1;
+			}
+
+			if (o1 > o2) {
+				return 1;
+			}
+
+			return 0;
+		}
+	};
+	final private BinaryOperator<Integer> costAdder = (Integer a, Integer b) -> a + b;
 
 	private int enemyMoves = 3;
 
@@ -55,38 +79,43 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 		enemies.add(new Enemy(enemyMoves, 10, 10, 25, 8, 10, 10, Color.RED));
 		enemies.add(new Enemy(enemyMoves, 15, 15, 25, 8, 10, 10, Color.RED));
 		enemies.add(new Enemy(enemyMoves, 10, 15, 25, 8, 10, 10, Color.RED));
-		grid = new GraphMatrix<Integer, Integer>(20, 20, 0, 1, FORBIDDEN, initialCost);
+		grid = new GraphMatrix<Integer, Integer>(sizeX, sizeY, EMPTY, VISITED, FORBIDDEN, initialCost, minimumCost,
+				maximumCost, costComparator, costAdder);
 		preview = new ArrayList<Position>();
-		
+
 		// Dicionário de custo/cores
 		HashMap<Integer, Color> hash = new HashMap<Integer, Color>();
 		hash.put(initialCost, Color.GREEN);
-		hash.put(initialCost+1, Color.YELLOW);
-		hash.put(initialCost+2, Color.ORANGE);
-		hash.put(initialCost+3, Color.MAGENTA);
-		
+		hash.put(initialCost + 1, Color.YELLOW);
+		hash.put(initialCost + 2, Color.ORANGE);
+		hash.put(initialCost + 3, Color.MAGENTA);
+
 		map = new Map(grid, hash, WIDTH, HEIGHT, 20, 20);
-		addRandomCosts(10, hash.size()); 
-		addRandomForbidden(20); 
-		
+		addRandomCosts(20, hash.size());
+		addRandomForbidden(20);
+
+		grid.setElementCost(player.getGridX(), player.getGridY(), initialCost);
+		grid.setElementValue(player.getGridX(), player.getGridY(), EMPTY);
+
 		start();
 	}
+
 	// Altera o custo de até <number> casas aleatórias(Temporário)
 	private void addRandomCosts(int number, int max) {
-		for(int i=1;i<=number;i++) {
+		for (int i = 1; i <= number; i++) {
 			int randomX = ThreadLocalRandom.current().nextInt(0, 20);
 			int randomY = ThreadLocalRandom.current().nextInt(0, 20);
 			int randomCost = ThreadLocalRandom.current().nextInt(1, 4);
 			grid.setElementCost(randomX, randomY, randomCost);
 		}
 	}
+
 	// Adiciona até <number> obstáculos intransponíveis(Temporário)
 	private void addRandomForbidden(int number) {
-		for(int i=1;i<=number;i++) {
+		for (int i = 1; i <= number; i++) {
 			int randomNumA = ThreadLocalRandom.current().nextInt(0, 20);
 			int randomNumB = ThreadLocalRandom.current().nextInt(0, 20);
 			grid.setElementValue(randomNumA, randomNumB, FORBIDDEN);
-			grid.setElementCost(randomNumA, randomNumB, FORBIDDEN);
 		}
 	}
 
@@ -117,7 +146,6 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 	public void stop() {
 		running = false;
 	}
-
 
 	@Override
 	public void mouseMoved(MouseEvent m) {
@@ -168,7 +196,7 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 			repaint();
 		}
 	}
-	
+
 	@Override
 	public void mouseEntered(MouseEvent m) {
 		mouseMoved(m);
@@ -193,7 +221,7 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 	@Override
 	public void mouseDragged(MouseEvent m) {
 	}
-	
+
 	private void drawPreview(Graphics2D g) {
 		int x = -1;
 		int y = -1;
@@ -212,23 +240,53 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 			}
 		}
 	}
-	
+
 	private boolean isForbidden(MouseEvent m) {
-		if(grid.getElementValue(coordToGrid(m.getX()), coordToGrid(m.getY())) == FORBIDDEN)
+		if (grid.getElementValue(coordToGrid(m.getX()), coordToGrid(m.getY())).equals(grid.getFORBIDDEN()))
 			return true;
 		else
 			return false;
 	}
 
+	private List<Position> cheapestPathToList(CheapestPath<Position, Integer> cpt) {
+		if (cpt == null) {
+			return new ArrayList<Position>();
+		}
+
+		return cpt.getPath();
+	}
+
 	private void encontraCaminho() {
-		preview = grid.bfs(new Position(player.getGridX(), player.getGridY()), new Position(lastMouseX, lastMouseY));
+		grid.setVisitedToEmpty();
+
+		// Ajuda a não entrar nos inimigos
+		for (Enemy e : enemies) {
+			grid.setElementValue(e.getGridX(), e.getGridY(), FORBIDDEN);
+		}
+
+		CheapestPath<Position, Integer> cpt = grid.dijkstra(new Position(player.getGridX(), player.getGridY()),
+				new Position(lastMouseX, lastMouseY));
+		preview = cheapestPathToList(cpt);
+
+		// Reverter modificação
+		for (Enemy e : enemies) {
+			grid.setElementValue(e.getGridX(), e.getGridY(), EMPTY);
+		}
+
 		grid.setVisitedToEmpty();
 	}
 
 	private void encontraCaminhoInimigos() {
 		for (Enemy enemy : enemies) {
-			List<Position> caminho = grid.bfs(new Position(enemy.getGridX(), enemy.getGridY()),
-					new Position(player.getGridX(), player.getGridY()));
+			// Impedir inimigos de entrarem uns nos outros
+			for (Enemy otherEnemy : enemies) {
+				grid.setElementValue(otherEnemy.getGridX(), otherEnemy.getGridY(), FORBIDDEN);
+			}
+
+			grid.setElementValue(enemy.getGridX(), enemy.getGridY(), EMPTY);
+
+			List<Position> caminho = cheapestPathToList(grid.dijkstra(new Position(enemy.getGridX(), enemy.getGridY()),
+					new Position(player.getGridX(), player.getGridY())));
 			if (!caminho.isEmpty()) {
 				if (caminho.size() > enemy.getMoves()) {
 					enemy.setGridX(caminho.get(enemy.getMoves()).getPosX());
@@ -238,6 +296,12 @@ public class Panel extends JPanel implements MouseListener, MouseMotionListener 
 					enemy.setGridY(caminho.get(caminho.size() - 1).getPosY());
 				}
 			}
+
+			// Reverter mudança
+			for (Enemy otherEnemy : enemies) {
+				grid.setElementValue(otherEnemy.getGridX(), otherEnemy.getGridY(), EMPTY);
+			}
+
 			grid.setVisitedToEmpty();
 		}
 	}
